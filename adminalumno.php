73618ea440891ac4b4bd1c17bc146e103e877ac3
arielfrod <arielfrod@gmail.com>
2021-07-26 21:07:50 -0300

    Conexion db ok

--- adminalumno.php
+++ adminalumno.php
@@ -1,21 +1,21 @@
-<?php require_once("connection.php");?>
+<?php require_once("./mysql/conexion.php"); ?>
 
-<?php require_once("head.php");?>
+<?php require_once("head.php"); ?>
 
-<?php require_once("header.php");?>
-    <center>
-        <h1 class="mb-5">Panel admin Alumnos</h1>
-        <table class="table">
-            <thead>
+<?php require_once("header.php"); ?>
+<center>
+    <h1 class="mb-5">Panel admin Alumnos</h1>
+    <table class="table">
+        <thead>
             <tr>
-                <th scope="col" >ID</th>
-                <th scope="col" >Nombre</th>
-                <th scope="col" >DNI</th>
+                <th scope="col">ID</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">DNI</th>
                 <th scope="col" colspan="3">Cursos</th>
             </tr>
 
-            </thead>
-            <tbody>
+        </thead>
+        <tbody>
             <tr>
                 <th scope="row">1</th>
                 <td>Mark</td>
@@ -24,13 +24,12 @@
                 <td rowspan="1"><button type="button" class="btn btn-secondary btn col-4">Modificar</button></td>
                 <td rowspan="1"><button type="button" class="btn btn-danger btn col-4">Eliminar</button></td>
             </tr>
-            </tbody>
-        </table>
+        </tbody>
+    </table>
 
-        <div class="row container"  >
-            <button type="button" class="btn btn-primary btn col-2">Agregar</button>
-        </div>
+    <div class="row container">
+        <button type="button" class="btn btn-primary btn col-2">Agregar</button>
+    </div>
 
-    </center>
+</center>
 <?php require_once("endofpage.php");
-
--- mysql/conexion.php
+++ mysql/conexion.php
@@ -1,14 +1,41 @@
 
 <?php
+$server = "localhost";
+$user = "root";
+$password = "";
+$database = "pfpdb";
 
-// $mysqli = new mysqli("127.0.0.1", "root", "", "veterinaria", 3306);
-// if ($mysqli->connect_errno) {
-//     echo "Hubo un error: " . $mysqli->connect_errno . " " . $mysqli->connect_error;
-// }
 
-// echo $mysqli->host_info . "\n";
+try {
+    $conexiondb = new PDO(
+        "mysql:host=$server;dbname=$database",
+        $user,
+        $password
+    );
+    $conexiondb->setAttribute(
+        PDO::ATTR_ERRMODE,
+        PDO::ERRMODE_EXCEPTION
+    );
+    ////////////
 
-// Establecer conexion mediante PDO(PHP Data Object)
+    $id = 2;
+    $respuesta = $conexiondb->prepare("SELECT * FROM persona where id=?  LIMIT 2");
+    $respuesta->execute(array($id));
+    $datos = $respuesta->fetchAll();
+
+    #El metodo errorInfo( ) captura el mensaje de error de la ultima consulta ejecutada en el servidor... siempre devuelve un array con 3 elementos, si no hay error, el primer elemento es el string 00000
+    #var_dump($cnx->errorInfo( ) );
+    var_dump($respuesta);
+    foreach ($datos as $r) {
+        var_dump($r);
+    }
+    //////////
+
+
+    //code...
+} catch (PDOException $error) {
+    var_dump("error de coneccion. ", $error->getMessage());
+}
 
 
 ?>
