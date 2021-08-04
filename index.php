3ea4c569118bcd0858ea35c2345f865fc68df066
arielfrod <arielfrod@gmail.com>
2021-08-04 18:21:10 -0300

    Version00

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
+
 <!-- Crear un CRUD para los cursos de formacion profesional, donde podamos inscribir y registrar a los
 alumnos en los distintos cursos en BBDD-->
 <!-- front del CRUD -->
-<?php require_once("connection.php");?>
 
-<?php require_once("head.php");?>
+<?php require_once("./mysql/conexion.php"); ?>
 
-<?php require_once("header.php");?>
+<?php require_once("head.php"); ?>
+
+<?php require_once("header.php"); ?>
 
 <center>
     <img src="img/pfp02.png" alt="" height="45%" width="45%">
 </center>
 
 <?php require_once("endofpage.php");
-
